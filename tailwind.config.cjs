cb1940436ad277721ea1b27d1f95a51f7811bb45
futurGH <me@futuristick.ca>
2023-11-23 20:28:57 -0500

    Add typeface

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,13 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import("tailwindcss").Config} */
 module.exports = {
 	darkMode: "class",
 	content: ["./src/**/*.{html,svelte,js,ts}"],
 	theme: {
+		fontFamily: {
+			sans: ["'General Sans'", ...defaultTheme.fontFamily.sans],
+		},
 		extend: {
 			fontSize: {
 				"label": "0.8125rem",
