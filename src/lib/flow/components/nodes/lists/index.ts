03c87dbf76f6f4fcf94540ac1c91b0a3de8c1150
futurGH <me@futuristick.ca>
2023-12-04 19:07:22 -0500

    Fix case

--- src/lib/flow/components/nodes/lists/index.ts
+++ src/lib/flow/components/nodes/lists/index.ts
@@ -2,7 +2,7 @@ import * as UserListNode from "./UserListNode.svelte";
 import * as ModListNode from "./ModListNode.svelte";
 
 export const listNodes = {
-	userlist: UserListNode,
+	userList: UserListNode,
 	modList: ModListNode,
 };
 export { UserListNode, ModListNode };
